dd6d98de5d8a27dcbad34af51f33fe050b5a26a0
maxwellmg <max@vale.network>
2023-08-15 16:59:51 -0400

    added playground, continued on main

---
+++ nfl_survivor_main.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+theoretical_hash = {PHL1: 100, NYG1: 150, DAL1: 300, WAS1: -150, PHL2: 101, NYG2: 151, DAL2: 301, WAS2: -151, PHL3: 102, NYG3: 152, DAL3: 302, WAS3: -152}
+
+best_path_list = []
+
+3.downto(1) do
+  greatest_value = 0
+  for team, odds in theoretical_hash
+    if odds > greatest_value
+      greatest_value = odds
+    end
+  for team, odds in theoretical_hash
+    if odds == greatest_value
+      best_path_list << team
+      week = team[3]
+      city = team[0, 2]
+      for team, odds in theoretical_hash
+        if team.start_with? city
+          theoretical_hash.remove! team, odds
+          #|| team.end_with? week
+        end
+      end
+    end
+  end
+end
+end
+
+
+
+puts best_path_list
--- nfl_survivor_playground.rb
+++ nfl_survivor_playground.rb
@@ -1,19 +1,2 @@
-#!/usr/bin/env ruby
-
 theoretical_hash = {PHL1: 100, NYG1: 150, DAL1: 300, WAS1: -150, PHL2: 101, NYG2: 151, DAL2: 301, WAS2: -151, PHL3: 102, NYG3: 152, DAL3: 302, WAS3: -152}
 
-best_path_list = []
-greatest_value = 0
-3.downto(1) do
-  for team, odds in theoretical_hash
-    if odds > greatest_value
-      greatest_value = odds
-    best_path_list << team
-    week = team[3]
-    #city = team[:2]
-    puts week
-    #puts city
-  end
-end
-end
-puts best_path_list
