d650a207e9e1e1e19713863973ec35617748bd42
maxwellmg <max@vale.network>
2023-09-05 12:47:36 -0400

    changes made to reflect minus odds as better

--- full_season_hash.rb
+++ full_season_hash.rb
@@ -1,2 +1,37 @@
 #!/usr/bin/env ruby
-full_season_hash = {}
+
+full_team_to_abbrev = {
+    "ATL" => "Atlanta", 
+    "BUF" => "Buffalo", 
+    "CHI" => "Chicago", 
+    "CIN" => "Cincinatti", 
+    "CLE" => "Cleveland", 
+    "DAL" => "Dallas", 
+    "DEN" => "Denver", 
+    "GB" => "Green Bay",
+    "TEN" => "Tennessee", 
+    "IND" => "Indianapolis", 
+    "KC" => "Kansas City",
+    "LV" => "Las Vegas",
+    "LAR" => "Los Angeles Rams", 
+    "MIA" => "Miami", 
+    "MIN" => "Minnesota", 
+    "NE" => "New England",
+    "NO" => "New Orleans",
+    "NYG" => "New York Giants", 
+    "NYJ" => "New York Jets", 
+    "PHI" => "Philadelphia", 
+    "ARI" => "Arizona", 
+    "PIT" => "Pittsburgh", 
+    "LAC" => "Los Angeles Chargers", 
+    "SF" => "San Francisco",
+    "SEA" => "Seattle", 
+    "TB" => "Tampa Bay",
+    "WSH" => "Washington", 
+    "CAR" => "Carolina", 
+    "JAX" => "Jacksonville", 
+    "BAL" => "Baltimore", 
+    "HOU"  => "Houston" 
+}
+
+full_season_hash = {"ATL1" => -179, }
