e42a32fff915efddc314f63b50bf795208607613
maxwellmg <max@vale.network>
2023-09-13 16:58:22 -0400

    code makes full call to api

--- odds_api_call.py
+++ odds_api_call.py
@@ -5,7 +5,16 @@ load_dotenv()
 
 API_KEY = os.getenv('ODDS_API_KEY')
 
-#example
-response = requests.get(f"https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions=us&markets=h2h&oddsFormat=american&apiKey={API_KEY}")
+SPORT = "americanfootball_nfl"
+MARKET = "h2h&odds"
+REGION = "us"
+ODDS_FORMAT = "american"
 
-print(response)
+#example
+response = requests.get(f"https://api.the-odds-api.com/v4/sports/{SPORT}/odds/?regions={REGION}&markets={MARKET}Format=american&apiKey={API_KEY}")
+
+if response.status_code != 200:
+    print(f'Failed to get sports: status_code {sports_response.status_code}, response body {sports_response.text}')
+else:
+    print ('List of NFL games:',
+    response.json())
