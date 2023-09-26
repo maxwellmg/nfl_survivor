e111e698fb26f1e4752fc32c5c3ae22854a8e82f
maxwellmg <max@vale.network>
2023-09-26 13:56:55 -0400

    adding function to take away chosen teams

---
+++ custom_methods_playground.rb
@@ -0,0 +1,12 @@
+def weeks_played?()
+    teams_selected = []
+    puts "Has the NFL Regular Season started? (Answer either 'y' or 'n')"
+    resp = gets.chomp
+    while length.teams_selected == 0
+      if resp == "y"
+  
+      elsif resp == "n"
+        puts "Okay! Starting from scratch then."
+  
+      else
+        puts ""
