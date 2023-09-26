2f292e223cb93ea758e92a28ddf3a860a0e8216e
maxwellmg <max@vale.network>
2023-09-26 13:58:30 -0400

    small edits on api call py

--- odds_api_call.py
+++ odds_api_call.py
@@ -11,10 +11,12 @@ REGION = "us"
 ODDS_FORMAT = "american"
 
 #example
-response = requests.get(f"https://api.the-odds-api.com/v4/sports/{SPORT}/odds/?regions={REGION}&markets={MARKET}Format=american&apiKey={API_KEY}")
+#response = requests.get(f"https://api.the-odds-api.com/v4/sports/{SPORT}/odds/?regions={REGION}&markets={MARKET}Format=american&apiKey={API_KEY}")
 
-if response.status_code != 200:
+response = f"https://api.the-odds-api.com/v4/sports/{SPORT}/odds/?regions={REGION}&markets={MARKET}Format=american&apiKey={API_KEY}"
+print(response)
+'''if response.status_code != 200:
     print(f'Failed to get sports: status_code {sports_response.status_code}, response body {sports_response.text}')
 else:
     print ('List of NFL games:',
-    response.json())
+    response.json())'''
